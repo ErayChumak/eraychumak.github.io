97f0b888f9167c0dd6820ab07772044a09785911
Eray Chumak <eray_chumak@protonmail.com>
2023-05-08 00:45:45 +0100

    Handle blob respawn server-side

--- public/classes/blob.js
+++ public/classes/blob.js
@@ -2,19 +2,28 @@ class FoodBlob {
   constructor(id, r, color, pos) {
     this.id = id;
     this.r = r;
+    this.eaten = false;
     this.color = color;
     this.pos = createVector(pos.x, pos.y);
   }
 
+  respawn(newPos) {
+    this.pos = createVector(newPos.x, newPos.y);
+    this.eaten = false;
+  }
+
+  hide() {
+    this.eaten = true;
+  }
+
   draw() {
-    if (this.pos.x && this.pos.y) {
-      fill(this.color);
-      ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
-    }
+    if (this.eaten) return;
+
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
 
     if (this.isTouching(player)) {
       socket.emit("blobEaten", this.id);
-      player.grow(this.r);
     }
   }
 

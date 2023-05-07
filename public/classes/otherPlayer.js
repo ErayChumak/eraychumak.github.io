0e4d9646aa2231d686d8a99b64b0b74ee0e03b9e
Eray Chumak <eray_chumak@protonmail.com>
2023-05-08 00:46:18 +0100

    Draw boundaries for debugging

--- public/classes/otherPlayer.js
+++ public/classes/otherPlayer.js
@@ -8,31 +8,45 @@ class OtherPlayer {
     this.r = player.r;
     this.minR = 64;
     this.pos = createVector(player.pos.x, player.pos.y);
-    this.color = `rgba(${round(random(255))}, ${round(random(255))}, ${round(random(255))}, 1)`;
+
+    this.color = `rgb(${this.rgb.r}, ${round(this.rgb.g)}, ${this.rgb.b})`;
+    this.darkerColor = `rgb(${this.rgb.r - 50}, ${round(this.rgb.g - 50)}, ${this.rgb.b - 50})`;
 
     allPlayersArrangement.push(this);
     allPlayersArrangement = allPlayersArrangement.sort((p1, p2) => p1.r - p2.r);
   }
 
   draw() {
-    stroke(255);
-    strokeWeight(MAP.zoom * 4);
-    fill(this.color);
-
     if (newPoses[this.id]) {
       this.pos.lerp(newPoses[this.id], .1);
     }
 
-    ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+    const strokeSize = MAP.zoom * 4;
 
-    noStroke();
-    strokeWeight(0);
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+    fill(this.darkerColor);
+    ellipse(this.pos.x, this.pos.y, (this.r * 2) - (strokeSize), this.r * 2 - (strokeSize));
+
     fill(0);
     textAlign(CENTER);
-    textSize(this.r / 4);
-    text(round(this.r), this.pos.x, this.pos.y + (this.r / 3));
+
+    // name
     textSize(this.r / 3);
     text(this.name, this.pos.x, this.pos.y);
+
+    // points
+    textSize(this.r / 4);
+    text(round(this.r), this.pos.x, this.pos.y + (this.r / 3));
+
+    stroke(255);
+    strokeWeight(2);
+    line(this.pos.x - this.r, this.pos.y - this.r, this.pos.x + this.r, this.pos.y - this.r); // top
+    line(this.pos.x - this.r, this.pos.y + this.r, this.pos.x + this.r, this.pos.y + this.r); // bottom
+    line(this.pos.x - this.r, this.pos.y - this.r, this.pos.x - this.r, this.pos.y + this.r); // left
+    line(this.pos.x + this.r, this.pos.y - this.r, this.pos.x + this.r, this.pos.y + this.r); // right
+    fill(255);
+    text(round(this.r * 2), this.pos.x, this.pos.y + this.r + (this.r * .5));
 
     if (this.isEdibleBy(player) && !this.dead) {
       socket.emit("eat", this.id);

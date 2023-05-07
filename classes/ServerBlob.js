a63f8b7f8dcea2d90b19823aa538d6e8b0c3ec4c
Eray Chumak <eray_chumak@protonmail.com>
2023-05-08 00:43:33 +0100

    Randomise blob size

--- classes/ServerBlob.js
+++ classes/ServerBlob.js
@@ -1,16 +1,18 @@
 class ServerBlob {
   constructor(id) {
     this.id = id;
-    this.r = 16;
+    this.r = Math.random() * (18 - 8) + 8;
     this.color = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
-    this.randomise();
+    this.randomisePos();
   }
 
-  randomise() {
+  randomisePos() {
     this.pos = {
       x: Math.random() * 5000,
       y: Math.random() * 5000
     }
+
+    return this.pos;
   }
 }
 
